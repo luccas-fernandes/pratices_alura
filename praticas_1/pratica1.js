76763ce05dd7bb43e25a545ba4462b9b4232c0fd
Luccas Fernandes <115430394+luccas-fernandes@users.noreply.github.com>
2024-03-25 11:54:58 -0300

    Add files via upload

---
+++ praticas_1/pratica1.js
@@ -0,0 +1,15 @@
+alert('Boas vindas ao nosso site!');
+//let nome = 'Lua';
+// let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos.')
+
+let mensagemDeErro = 'Erro! Preencha todos os campos'
+
+let nome  = prompt('Informe seu nome')
+let idade = prompt('Informe sua idade')
+
+alert('Seu nome é: ', {nome})
+alert('Sua idade é: ', {idade})
